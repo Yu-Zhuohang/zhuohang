6dd43db0eea08d4789147e75000f457290d54062
YuZhuohang <14292320+yuzhuohang@user.noreply.gitee.com>
2025-04-17 21:43:26 +0800

    clear all

--- js/function/mail.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const message = document.getElementById('message').value;
-
-    const mailtoLink = `mailto:yuzhuohang2004@foxmail.com?subject=Message from ${name}&body=From: ${email}%0D%0A%0D%0A${message}`;
-    window.location.href = mailtoLink;
-});
